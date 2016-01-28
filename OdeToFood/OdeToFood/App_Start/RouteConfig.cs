521c7b7f81fd94ed6420afce6ad344eea929dc4a
Alexej Belov <grifon102@ya.ru>
2016-01-28 23:54:24 +0300

    Add action and params

--- OdeToFood/OdeToFood/App_Start/RouteConfig.cs
+++ OdeToFood/OdeToFood/App_Start/RouteConfig.cs
@@ -12,7 +12,10 @@ namespace OdeToFood
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute("Cuisine",
+                "cuisine/{name}",
+                new {controller = "Cuisine", action = "Search", name=UrlParameter.Optional });
+            //Home
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
